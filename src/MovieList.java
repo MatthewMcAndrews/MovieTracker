cd2bae03c3d80f2c1195297f1ffd62751a2aa23e
Matthew M <MatthewFMcAndrews@gmail.com>
2021-08-25 16:55:02 -0500

    Initial commit

---
+++ src/MovieList.java
@@ -0,0 +1,11 @@
+public class MovieList {
+    private int numberOfMovies = 0;
+
+    public int size() {
+        return numberOfMovies;
+    }
+
+    public void Add(Movie movieToAdd) {
+        numberOfMovies = 1;
+    }
+}
